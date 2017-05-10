8c6663dd64899f060ae40de7c601c6efd93117a0
sagarvadn <sagar.vadnere@hbwsl.com>
2017-05-10 12:14:04 +0530

    Removed WooCommerce gallery support code

--- functions.php
+++ functions.php
@@ -463,12 +463,4 @@ function cyberchimps_wpforms_install_link() {
 
 	return wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=' . $slug ), 'install-plugin_' . $slug );
 }
-
-add_action( 'after_setup_theme', 'parallax_woocommerce_gallery_support' );
- 
-function parallax_woocommerce_gallery_support() {
-    add_theme_support( 'wc-product-gallery-zoom' );
-    add_theme_support( 'wc-product-gallery-lightbox' );
-    add_theme_support( 'wc-product-gallery-slider' );
-}
 ?>
